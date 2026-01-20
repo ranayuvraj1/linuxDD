9690448ae3418b6618a97cf2dbf7aad9cd3c9b74
ranayuvraj1 <rana.yuvraj@truminds.com>
2026-01-20 08:40:04 +0530

    Update test.c

--- yuvraj/AdvanceCharDriver/test.c
+++ yuvraj/AdvanceCharDriver/test.c
@@ -1,29 +1,33 @@
 #include <stdio.h>
 #include <fcntl.h>
-#include <sys/ioctl.h>
 #include <unistd.h>
+#include <sys/ioctl.h>
 
-#define MYDRV_SET_TIMER_VAL  _IOW('a', 1, int)
-#define MYDRV_ENABLE_TIMER  _IO('a', 2)
-#define MYDRV_STOP_TIMER    _IO('a', 3)
+#define MY_MAGIC 'M'
+#define IOCTL_GET_VAL _IOR(MY_MAGIC, 1, int)
+#define IOCTL_SET_VAL _IOW(MY_MAGIC, 2, int)
+#define IOCTL_CLR_VAL _IO(MY_MAGIC, 3)
 
 int main()
 {
-    int fd, t = 2;
+    int fd, val;
 
-    fd = open("/dev/mydrv", O_RDWR);
-    if (fd < 0) {
+    fd = open("/dev/mychardev", O_RDWR);
+    if (fd < 0)
+    {
         perror("open");
         return 1;
     }
 
-    ioctl(fd, MYDRV_SET_TIMER_VAL, &t);
-    ioctl(fd, MYDRV_ENABLE_TIMER);
+    val = 100;
+    ioctl(fd, IOCTL_SET_VAL, &val);
 
-    sleep(5);
+    val = 0;
+    ioctl(fd, IOCTL_GET_VAL, &val);
+    printf("GET_VAL = %d\n", val);
 
-    ioctl(fd, MYDRV_STOP_TIMER);
+    ioctl(fd, IOCTL_CLR_VAL);
+
     close(fd);
-
     return 0;
 }
