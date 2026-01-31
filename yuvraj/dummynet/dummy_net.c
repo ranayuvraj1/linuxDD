5d0fdcb36b343c5c3468b53bf1ff41fc38a39c9b
ranayuvraj1 <rana.yuvraj@truminds.com>
2026-01-31 14:54:49 +0530

    Update dummy_net.c
    All Fixes related to the missing function

--- yuvraj/dummynet/dummy_net.c
+++ yuvraj/dummynet/dummy_net.c
@@ -14,7 +14,6 @@ static int major;
 static struct class *hyd_class;
 static struct device *hyd_device;
 
-// sysfs logs
 static char eth0_log[4096];
 static char eth1_log[4096];
 static int eth0_pos, eth1_pos;
@@ -24,8 +23,6 @@ struct ioc_req
     int if_id;
     int len;
 };
-
-// SYSFS
 
 static ssize_t eth0_show(struct kobject *k,
                          struct kobj_attribute *a, char *b)
@@ -42,8 +39,6 @@ static ssize_t eth1_show(struct kobject *k,
 static struct kobj_attribute e0 = __ATTR(eth0, 0444, eth0_show, NULL);
 static struct kobj_attribute e1 = __ATTR(eth1, 0444, eth1_show, NULL);
 
-// NET OPS
-
 static int hyd_open(struct net_device *d)
 {
     netif_start_queue(d);
@@ -51,25 +46,63 @@ static int hyd_open(struct net_device *d)
     return 0;
 }
 
+static int hyd_stop(struct net_device *d)
+{
+    netif_stop_queue(d);
+    printk("stop %s\n", d->name);
+    return 0;
+}
+
+static void hyd_uninit(struct net_device *d)
+{
+    printk("uninit %s\n", d->name);
+}
+
+static void hyd_rx(struct sk_buff *skb)
+{
+    skb->protocol = eth_type_trans(skb, skb->dev);
+
+    skb->dev->stats.rx_packets++;
+    skb->dev->stats.rx_bytes += skb->len;
+
+    netif_rx(skb);
+}
+
 static netdev_tx_t hyd_xmit(struct sk_buff *skb, struct net_device *d)
 {
     int id = (d == devs[0]) ? 0 : 1;
+    struct ethhdr *eth;
+    unsigned char *payload;
 
-    printk("xmit %s len=%u\n", d->name, skb->len);
+    if (!netif_running(d))
+    {
+        dev_kfree_skb(skb);
+        return NETDEV_TX_OK;
+    }
+
+    d->stats.tx_packets++;
+    d->stats.tx_bytes += skb->len;
+
+    eth = eth_hdr(skb);
+    if (eth)
+        printk("xmit %s src %pM dst %pM len=%u\n",
+               d->name, eth->h_source, eth->h_dest, skb->len);
+
+    payload = skb->data;
+    printk("first payload byte data: %02x\n", payload[0]);
 
     skb->dev = devs[1 - id];
-    skb->protocol = eth_type_trans(skb, skb->dev);
-    netif_rx(skb);
+    hyd_rx(skb);
 
     return NETDEV_TX_OK;
 }
 
 static const struct net_device_ops ops = {
     .ndo_open = hyd_open,
+    .ndo_stop = hyd_stop,
     .ndo_start_xmit = hyd_xmit,
+    .ndo_uninit = hyd_uninit,
 };
-
-// IOCTL
 
 static long hyd_ioctl(struct file *f, unsigned int c, unsigned long a)
 {
@@ -79,8 +112,9 @@ static long hyd_ioctl(struct file *f, unsigned int c, unsigned long a)
     char line[64];
     int n;
     struct sk_buff *skb;
+    struct ethhdr *eth;
 
-    if (copy_from_user(&req, (void *)a, sizeof(req)))
+    if (copy_from_user(&req, (void __user *)a, sizeof(req)))
         return -EFAULT;
 
     if (req.if_id < 0 || req.if_id > 1)
@@ -109,13 +143,22 @@ static long hyd_ioctl(struct file *f, unsigned int c, unsigned long a)
     }
 
     skb = alloc_skb(req.len + ETH_HLEN, GFP_KERNEL);
+    if (!skb)
+        return -ENOMEM;
+
     skb_reserve(skb, ETH_HLEN);
     skb_put(skb, req.len);
 
-    skb->dev = devs[req.if_id];
-    skb->protocol = eth_type_trans(skb, skb->dev);
+    skb_push(skb, ETH_HLEN);
+    skb_reset_mac_header(skb);
 
-    netif_rx(skb);
+    eth = eth_hdr(skb);
+    eth->h_proto = htons(ETH_P_IP);
+    eth_zero_addr(eth->h_source);
+    eth_zero_addr(eth->h_dest);
+
+    skb->dev = devs[req.if_id];
+    hyd_rx(skb);
 
     return 0;
 }
@@ -124,13 +167,11 @@ static struct file_operations fops = {
     .unlocked_ioctl = hyd_ioctl,
 };
 
-// INIT / EXIT
-
 static int __init hyd_init(void)
 {
     major = register_chrdev(0, "hyd", &fops);
 
-    hyd_class = class_create(THIS_MODULE, "hyd");
+    hyd_class = class_create("hyd");
     hyd_device = device_create(hyd_class, NULL,
                                MKDEV(major, 0), NULL, "hyd");
 
@@ -147,12 +188,14 @@ static int __init hyd_init(void)
     register_netdev(devs[1]);
 
     kobj = kobject_create_and_add("hyd01", kernel_kobj);
-    sysfs_create_file(kobj, &e0.attr);
-    sysfs_create_file(kobj, &e1.attr);
 
-    major = register_chrdev(0, "hyd", &fops);
+    if (sysfs_create_file(kobj, &e0.attr))
+        printk("sysfs eth0 failed\n");
 
-    printk("hyd loaded\n");
+    if (sysfs_create_file(kobj, &e1.attr))
+        printk("sysfs eth1 failed\n");
+
+    printk("Hyd loading Done\n");
     return 0;
 }
 
@@ -162,9 +205,15 @@ static void hyd_exit(void)
     class_destroy(hyd_class);
 
     unregister_chrdev(major, "hyd");
+
     unregister_netdev(devs[0]);
     unregister_netdev(devs[1]);
+
+    free_netdev(devs[0]);
+    free_netdev(devs[1]);
+
     kobject_put(kobj);
+
     printk("hyd removed\n");
 }
 
